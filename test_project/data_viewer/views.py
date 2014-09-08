99f5b8cbedc3f92fbc405355b207e5b7b6b9d3af
Pawel Krolikowski <pawel.krolikowski@geneity.co.uk>
2014-09-08 12:24:25 +0100

    Semi working html template

--- test_project/data_viewer/views.py
+++ test_project/data_viewer/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 def index(request):
 	return HttpResponse("Hello world, you are at the data viewer index")
 
 def csv(request):
-	return HttpResponse("CSV")
+	columns = ['123', '456']
+
+	template = loader.get_template('data_viewer/table_view.html')
+	context = RequestContext(request, { 
+        'columns': columns,
+	})
+	return HttpResponse(template.render(context), content_type='text/html')
 
 def prn(request):
 	return HttpResponse("PRN")
