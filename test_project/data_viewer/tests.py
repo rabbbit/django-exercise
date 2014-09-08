bcc878b1372207a279730d47bd808e3becd5798a
Pawel Krolikowski <pawel.krolikowski@geneity.co.uk>
2014-09-08 14:29:09 +0100

    Formatting/printing .prn file + test
    Didn't find any library to handle that file, did it myself.

--- test_project/data_viewer/tests.py
+++ test_project/data_viewer/tests.py
@@ -1,3 +1,62 @@
 from django.test import TestCase
+from data_viewer.views import _find_col_begins, _split_data
 
-# Create your tests here.
+class DataViewerTestCase(TestCase):
+
+	def test_col_begins_detection(self):
+		
+		to_test = (
+			(['123 234'], [4]),
+			(['123 234 345'], [4,8]),
+			(['123 234 345', '          '], [4,8]),
+			(
+				['123 234 345',
+				 '123 234 4353453534535'],
+				[4,8]
+			),
+			(
+				['123 234 345',
+				 '1232344 4353453534535'],
+				[8]
+			),
+			(
+				['123 234 345',
+				 '123 344 435',
+				 '123 3444 35'],
+				[4,8]
+			),
+		)
+
+		for columns, exp_output in to_test:
+			self.assertEquals(
+				_find_col_begins(columns),
+				exp_output,
+			)
+
+	def test_split_data(self):
+
+		to_test = (
+			(['123 123 123'], [4,8], [['123', '123', '123']]),
+			(['123123123'],   [3,6], [['123', '123', '123']]),
+			(
+				#input strings
+				['123123123',
+				 '123123123'],
+				#col begins
+				[3,6],
+				#output list-of-list split by col_begins
+				[
+					['123', '123', '123'],
+					['123', '123', '123']
+				]
+			),
+		)
+
+		for lines, col_begins, exp_output in to_test:
+			self.assertEquals(
+				_split_data(lines, col_begins),
+				exp_output,
+			)
+
+
+
--- test_project/data_viewer/views.py
+++ test_project/data_viewer/views.py
@@ -1,24 +1,26 @@
 import csv
 
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
 	return render(request, 'data_viewer/index.html')
 
 def csv_data(request):
+	col_names, data = get_csv_data()
+	return render_response(request, col_names, data)
 
-	columns, data = get_csv_data()
+def prn_data(request):
+	col_names, data = get_prn_data()
+	return render_response(request, col_names, data)
 
+
+def render_response(request, col_names, data):
 	context = {
-		'columns': columns,
+		'col_names': col_names,
 		'data': data,
 	}
 
 	return render(request, 'data_viewer/table_view.html', context)
-
-def prn_data(request):
-	return HttpResponse("PRN")
 
 
 def get_csv_data():
@@ -31,6 +33,91 @@ def get_csv_data():
 		col_names = next(reader)
 
 		for line in reader:
-			data.append(entry.decode('cp1252').encode('utf-8') for entry in line)
+			data.append(_make_encoding(entry) for entry in line)
 
 	return col_names, data
+
+
+def get_prn_data():
+
+	with open('data_viewer/data/Workbook2.prn') as f:
+		lines = f.readlines()
+
+	col_begins = _find_col_begins(lines)
+	data = _split_data(lines, col_begins)
+
+
+	return data[0], data[1:]
+
+
+def _make_encoding(entry):
+	"""
+		Hardcoded, but seems to work for given files
+	"""
+	return entry.strip().decode('cp1252').encode('utf-8')
+
+
+def _split_data(lines, col_begins):
+	"""
+		Given a list of strings, and list of integers.
+		splits each string into substrings, and return list of lists of those
+		substrings.
+		Example:
+		(['abc abc abc'], (4,8)) will return [['abc', 'abc', 'abc']]
+	"""
+	data = []
+	for line in lines:
+		prev_col_begin = 0
+		row = []
+
+		for col_begin in col_begins:
+			entry = line[prev_col_begin: col_begin]
+			entry = _make_encoding(entry)
+			row.append(entry)
+			prev_col_begin = col_begin
+
+		row.append(_make_encoding(line[col_begin:]))
+		data.append(row)
+
+	return data
+
+
+def _find_col_begins(lines):
+	"""
+		Finds col begins by looking at first row, finding potential
+		col begins, then validating them by iteratin over data set
+			col begin - index of col begin within a string,
+			potential col begin - location of every word in first row
+			a col begin is in-valid if any rows in the data set has character on 
+			that location, and one location on the left
+		Example:
+		123 123 123
+		12  1234 34
+		Col-begins = (0, 4, 8)
+
+		WARNING: 
+			- very in-efficient, keeps whole data in memory
+				and iterates over it.
+				For big data sets, find a better way.
+			- assumes that column names are unique
+	"""
+
+	first_line = lines[0]
+	col_names = lines[0].split()
+
+	col_begins = set()
+	for col_name in col_names:
+		col_begins.add(first_line.find(col_name))
+
+	col_begins.remove(0)
+
+	for line in lines:
+		false_begins = set()
+		for col_begin in col_begins:
+			if line[col_begin-1] != ' ' and line[col_begin] != ' ':
+				false_begins.add(col_begin)
+		col_begins -= false_begins
+
+	return sorted(col_begins)
+
+
